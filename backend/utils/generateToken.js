b1eaff22c309f3dac6ea422bca4a3cebb259ccf7
Abhik-Mudi <abhikmudi6@gmail.com>
2026-01-15 20:34:26 +0530

    some changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (userId, res)=>{
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 15 * 24 * 60 * 60 *1000,
     })
 }
